155ce415f20a0bce9d1671541d97ff42e30b98fb
Szorad <david.szorad@accenture.com>
2017-09-19 16:04:40 +0200

    Initial commit

---
+++ Generics.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsInheritance
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T sut);
+    }
+
+    public class EligibleForDiscountSpecification : ISpecification<Customer>
+    {
+        private readonly Product _product;
+
+        public EligibleForDiscountSpecification(Product product)
+        {
+            _product = product;
+        }
+
+        public bool IsSatisfiedBy(Customer customer)
+        {
+            return (_product.Price < 100 && customer.CreditRating >= _product.MinimumCreditRating);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsInheritance
+{
+    public class Customer
+    {
+        public double CreditRating { get; set; }
+    }
+
+    public class Product
+    {
+        public double Price { get; set; }
+        public double MinimumCreditRating { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product();
+            var customer = new Customer();
+            var eligible = new EligibleForDiscountSpecification(product);
+            var satisfied = eligible.IsSatisfiedBy(customer);
+        }
+    }
+}
